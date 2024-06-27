3b5878a8589a8f14c8b11d5c007b90e198c26826
AzureAD\박인태 <pit1995@outlook.kr>
2024-06-27 11:22:59 +0900

    useEffect & Deps it

--- react-for-starter/src/App.js
+++ react-for-starter/src/App.js
@@ -1,11 +1,26 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
+
 
 function App() {
   const [counter, setValue] = useState(0);
+  const[keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time.");
+  useEffect(() => {
+    console.log("i run only onece.");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+      console.log("Search for", keyword ,"when only keyword is changed.");
+  }}, [keyword]);
+  useEffect(() => {
+      console.log("i run when 'counter' changes.");
+}, [counter]);
   return (
     <div>
+      <input
+        value={keyword} onChange={onChange} type="text" placeholder="Search here" />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>
