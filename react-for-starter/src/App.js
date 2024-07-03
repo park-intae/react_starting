ca98d247e6fd019664fdc74596eea3ae88c3d291
AzureAD\박인태 <pit1995@outlook.kr>
2024-07-03 23:20:02 +0900

    Code Challenge

--- react-for-starter/src/App.js
+++ react-for-starter/src/App.js
@@ -2,25 +2,53 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [USD, setUSD] = useState("");
   const [coins, setCoins] = useState([])
+  const onChange = (event) => setUSD(event.target.value);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
       .then((response) => response.json())
-      .then(json => setCoins(json));
-    setCoins(json);
-    setLoading(false);
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
+  const saveUSd = event => {
+    setUSD(event.target.value);
+  }
   return (
     <div>
       <h1>The Coins! ({loading ? "" : `(${coins.length})`})</h1>
-      {loading ? <strong>Loading...</strong> :(
-        <select>
-          {coins.map((coin) =>
+      <h3>USD to Coins</h3>
+      {loading ? <strong>Loading...</strong> : (
+        // <select>
+        //   {coins.map((coin) =>
+        //     <option>
+        //       {coin.name}({coin.symbol}) : {coin.quotes.USD.price} USD
+        //     </option>
+        //   )}
+        // </select>
+        <div>
+          <select>
+            Coin
             <option>
-              {coin.name}({coin.symbol}) : {coin.quotes.USD.price} USD
+              {coins.name}
             </option>
-          )}
-        </select>
+          </select>
+          <div>
+            <div>
+              USD
+            </div>
+            <input id="inputUSD" type="text" placeholder="Enter your USD" onChange={onChange} />.
+            <div>
+              Coin
+            </div>
+            <div type="text" value={setCoins(coins.quotes.USD.price) * setUSD.value} />
+          </div>
+          <div>
+
+          </div>
+        </div>
       )}
 
     </div>
