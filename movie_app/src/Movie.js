2a08040e5622a749efcf3b03d6f82a34f093f8df
AzureAD\박인태 <pit1995@outlook.kr>
2024-07-07 16:27:04 +0900

    React Router

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function Movie({coverImage, title, summary, genres}){
-    return  <div>
-    <img src={coverImage} alt="title"></img>
-    <h2>{title}</h2>
+function Movie({ coverImg, title, summary, genres }) {
+  return <div>
+    <img src={coverImg} alt="title"></img>
+    <h2>
+      <Link to="/movie">{title}</Link>
+    </h2>
     <p>{summary}</p>
     <ul>
       {genres.map((g) => (
@@ -14,10 +17,10 @@ function Movie({coverImage, title, summary, genres}){
 }
 
 Movie.prototype = {
-    coverImage: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genresL: PropTypes.arrayOf(PropTypes.string).isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
