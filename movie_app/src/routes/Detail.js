96e13c007a7dd3af763d2a5099e577ec2f326a79
AzureAD\박인태 <pit1995@outlook.kr>
2024-07-10 00:01:19 +0900

    Parameters

--- movie_app/src/routes/Detail.js
+++ movie_app/src/routes/Detail.js
@@ -1,4 +1,30 @@
-function Detail(){
-    return <h1>Detail</h1>;
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from './../Movie';
+
+function Detail() {
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const getMovies = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json);
+        setLoading(false);
+    }
+    useEffect(() => {
+        getMovies();
+    }, [])
+    return (
+        <div>
+            {loading ? (<h1>Loading...</h1>) : (
+                <div>
+                    <Movie                    
+                    
+                    />
+                </div>
+            )}
+        </div>
+    );
 }
 export default Detail;
